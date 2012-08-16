e6d38e7f38c2439ccb5e0a326644a42ed3be110c
Grady O'Connell <flipcoder@gmail.com>
2012-08-16 12:32:52 -0700

    fixed some stuff

---
+++ src/IScriptable.cpp
@@ -0,0 +1,15 @@
+#include <luabind/luabind.hpp>
+#include "IScriptable.h"
+#include "IFallible.h"
+
+IScriptable :: IScriptable(Script* script)
+{
+    if(!connect(script))
+        throw Failure();
+}
+
+bool IScriptable :: connect(Script* script)
+{
+    return true;
+}
+
